6401d5fcf93e8e6c16d4352fcedb1a6de8fc32e5
MICHAEL EMMANUEL <emmanuelmichaelpk3@gmail.com>
2025-10-18 06:10:22 +0100

    Tested database connection in test.php; everything is working

---
+++ src/tests/test.php
@@ -0,0 +1,21 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../database/database.php';
+
+// Create a new database object
+$db = new Database();
+
+// Get the PDO connection
+$conn = $db->getConnection();
+ 
+//Test if connection works
+if($conn){
+    echo "Database Connection successfull!";
+} else {
+    echo "Database connection fail!";
+}
+
+?>
