ff5b5808e44ea80705f7b65cdbaa3f929ee42a45
MICHAEL EMMANUEL <emmanuelmichaelpk3@gmail.com>
2025-10-27 04:06:11 +0100

    Add comment about database import in test.php

--- src/tests/test.php
+++ src/tests/test.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// import from database
 require_once __DIR__ . '/../database/database.php';
 
 // Create a new database object
